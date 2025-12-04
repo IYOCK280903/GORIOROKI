014723886de3fc70ac1fe87a800a7ff789dca9c1
roki24 <rokyagung2@gmail.com>
2025-12-05 01:19:00 +0700

    memperbaiki kelas event

---
+++ app/src/main/java/com/example/gorioroki_event/Models/Event.kt
@@ -0,0 +1,8 @@
+package com.example.gorioroki_event.models
+data class Event(
+    val id: Int? = null,
+    val title: String,
+    val date: String,
+    val location: String,
+    val description: String
+)
